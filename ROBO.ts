2dff0809530a06780e0ed63263e38e802111635a
Andkago <56820413+Andkago@users.noreply.github.com>
2019-11-09 13:18:28 +0100

    Updates.

--- ROBO.ts
+++ ROBO.ts
@@ -22,7 +22,7 @@ namespace ROBO {
     //% block
     export function SpillLyd(Tid: number): void {
         pins.analogSetPitchPin(AnalogPin.P9)
-        music.beginMelody(music.builtInMelody(Melodies.JumpUp), MelodyOptions.Once)
+        music.beginMelody(music.builtInMelody(Melodies.JumpUp), MelodyOptions.Forever)
         basic.pause(Tid)
     }
     //% block
